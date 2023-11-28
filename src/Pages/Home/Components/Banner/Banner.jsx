e529ccbedcf72cccdb94b28d321094c3eb894b9a
nasimrifat101 <nasimrifat101@gmail.com>
2023-11-28 22:18:05 +0600

    heading fixed

--- src/Pages/Home/Components/Banner/Banner.jsx
+++ src/Pages/Home/Components/Banner/Banner.jsx
@@ -3,7 +3,7 @@ const Banner = () => {
     <div className="h-[500px]">
       <div className="max-w-6xl mx-auto grid grid-cols-2">
         <div className="space-y-5 mt-28">
-          <h1 className="text-6xl font-bold">Inventory Management</h1>
+          <h1 className="text-7xl font-bold">Inventory Management</h1>
           <p className="w-[520px]">
             Elevate Efficiency with Our Inventory Management Solution!
             Streamline control, minimize errors, and maximize profits
