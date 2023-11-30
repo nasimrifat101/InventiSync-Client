0c4129b2b52955385b389bed7f49d8474e72d4d3
nasimrifat101 <nasimrifat101@gmail.com>
2023-11-30 16:25:12 +0600

    some minors fix

--- src/Pages/Home/Components/Banner/Banner.jsx
+++ src/Pages/Home/Components/Banner/Banner.jsx
@@ -1,10 +1,10 @@
 const Banner = () => {
   return (
-    <div className="h-[500px]">
+    <div className="h-[400px] lg:h-[500px]">
       <div className="max-w-6xl mx-auto grid lg:grid-cols-2">
         <div className="space-y-5 mt-24 p-3 lg:p-0">
-          <h1 className="text-4xl lg:text-8xl text-green-200 font-bold">Inventory Management</h1>
-          <p className="lg:w-[520px]">
+          <h1 className="text-4xl text-center lg:text-left lg:text-8xl text-green-200 font-bold">Inventory Management</h1>
+          <p className="lg:w-[520px] text-center lg:text-left">
             Elevate Efficiency with Our Inventory Management Solution!
             Streamline control, minimize errors, and maximize profits
             effortlessly. <span className="text-green-300 font-semibold">Explore now!</span>
